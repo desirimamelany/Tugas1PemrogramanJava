7edfe4f58865ad9b4668c8a48adfcdb76bd43b4c
desirimamelany <melanydesirima@yahoo.com>
2016-10-31 08:34:05 +0700

    12141365 Pada program ini inputan untuk nilai panjang ditentukan oleh user ketika program dijalankan (runtime). Untuk dapat menampung inputan ketika program dijalankan,  menggunakan bantuan Scanner. Setelah dilakukan proses instansiasi terhadap Scanner, baru dapat menggunakan fungsi nextInt() untuk menangkap nilai Integer (inputan nilai panjang).

---
+++ src/BintangSegitiga.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class BintangSegitiga {
+    public static void main(String[] args) {
+        int panjang;
+        Scanner input = new Scanner(System.in);
+        System.out.printf("Jumlah Karakter/Tinggi Segitiga : ");
+        panjang = input.nextInt();
+        System.out.print("-----------------------------------");
+
+        
+    }
+}
