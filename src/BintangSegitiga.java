5c338cb9171f3b1a8226d62264f796129df0bb7c
desirimamelany <melanydesirima@yahoo.com>
2016-10-31 08:55:48 +0700

    12141365 penambahan script segitiga siku kiri atas. Jalannya coding ketika looping for i=0 maka akan dicek dengan kondisi kurang dari panjang. Panjang didapatkan dari inputan parameter. Jika memenuhi maka akan masuk ke looping for j. Pada for j mulai dari j=0 dengan batas j<i, hal ini bertujuan agar pada baris pertama hanya menampilkan 1 bintang dan akan bertambah 1 bintang pada bariss dibawahnya. Dalam for j, bintang akan tercetak dari kiri ke kanan tidak akan ke bawah karena menggunakan sout. Setelah mencetak bintang maka j++ dan mulai for j lagig dengan nilai j sekarang 1 karena melebihi batas maka for j berhenti dan akan dienter karena ada perintah sout. Kemudian for pada i akan di i++ menjadi i=1. Hal ini akan terus dilakukan selama for i belum mencapai batas panjang.

--- src/BintangSegitiga.java
+++ src/BintangSegitiga.java
@@ -2,12 +2,21 @@ import java.util.Scanner;
 
 class BintangSegitiga {
     public static void main(String[] args) {
+        int i, j;
         int panjang;
         Scanner input = new Scanner(System.in);
         System.out.printf("Jumlah Karakter/Tinggi Segitiga : ");
         panjang = input.nextInt();
         System.out.print("-----------------------------------");
 
+        // Segitiga siku kiri atas      
+        for(i=0;i<=panjang;i++) {
+            for(j=0; j<i; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+        System.out.println();
         
     }
 }
